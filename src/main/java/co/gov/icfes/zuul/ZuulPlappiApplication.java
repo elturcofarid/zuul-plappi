1b5e3465fb2863db604efda24bca4c42494e7060
faridureche <faridureche@DESKTOP-4FEMPVJ>
2020-11-11 19:09:37 -0500

    commit

---
+++ src/main/java/co/gov/icfes/zuul/ZuulPlappiApplication.java
@@ -0,0 +1,13 @@
+package co.gov.icfes.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulPlappiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulPlappiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/gov/icfes/zuul/ZuulPlappiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.gov.icfes.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulPlappiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
